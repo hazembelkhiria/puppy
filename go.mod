070d02805094258357f228d0ccf1881fac43cab2
Hazem BELKHIRIA <hazembelkhiria@Hazems-Laptop.local>
2025-01-12 16:46:27 +0100

    four

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hazembelkhiria/puppy
 
 go 1.23.4
+
+require github.com/hazembelkhiria/dogs v0.0.0-20250112153423-57bb569cee08
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/hazembelkhiria/dogs"
+)
+
 func Bark() string {
 	return "woof!!"
 }
 
 func Barks() string {
 	return "woof!! woof!! woof!!"
+}
+
+func BigBark() string {
+	return dogs.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dogs.WhenGrownUp(Barks())
 }
